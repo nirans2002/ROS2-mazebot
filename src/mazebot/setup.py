24ff2f783ca93dd09334da595474d1481a9377ae
Niran S Narayanan <nirans2002@gmail.com>
2023-05-11 18:09:10 +0530

    launch file

--- src/mazebot/setup.py
+++ src/mazebot/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+from glob import glob
+import os
 package_name = 'mazebot'
 
 setup(
@@ -10,6 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'urdf'),glob('urdf/*')),
+        (os.path.join('share',package_name,'meshes'),glob('meshes/*')),
+        (os.path.join('share',package_name,'launch'),glob('launch/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
